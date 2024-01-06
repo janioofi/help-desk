c5685c346d70f8b1d5bfdf947a6acb34e5b4d359
Jânio <janioofi@gmail.com>
2024-01-05 21:21:59 -0300

    Perfil de desenvolvimento

--- back-end/src/main/java/com/janioofi/helpdesk/HelpdeskApplication.java
+++ back-end/src/main/java/com/janioofi/helpdesk/HelpdeskApplication.java
@@ -1,51 +1,15 @@
 package com.janioofi.helpdesk;
 
-import com.janioofi.helpdesk.domain.Chamado;
-import com.janioofi.helpdesk.domain.Cliente;
-import com.janioofi.helpdesk.domain.Tecnico;
-import com.janioofi.helpdesk.domain.enums.Perfil;
-import com.janioofi.helpdesk.domain.enums.Prioridade;
-import com.janioofi.helpdesk.domain.enums.Status;
-import com.janioofi.helpdesk.repositories.ChamadoRepository;
-import com.janioofi.helpdesk.repositories.ClienteRepository;
-import com.janioofi.helpdesk.repositories.TecnicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
 
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-    @Autowired
-    private TecnicoRepository tecnicoRepository;
-    @Autowired
-    private ClienteRepository clienteRepository;
-    @Autowired
-    private ChamadoRepository chamadoRepository;
+public class HelpdeskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        Tecnico tecnico = new Tecnico(null,"Jona Silveira", "66232536100", "jonas@gmail.com", "teste");
-        tecnico.addPefil(Perfil.ADMIN);
-
-        Cliente cliente = new Cliente(null, "Rubens Nascimento", "63185578147", "rubens@gmail.com", "teste");
-
-        Chamado c1 = new Chamado();
-        c1.setCliente(cliente);
-        c1.setTecnico(tecnico);
-        c1.setPrioridade(Prioridade.MEDIA);
-        c1.setTitulo("Ajustar Impressora");
-        c1.setStatus(Status.ANDAMENTO);
-        c1.setDataAbertura(LocalDate.now());
-        c1.setObservacoes("Testar");
-        tecnicoRepository.save(tecnico);
-        clienteRepository.save(cliente);
-        chamadoRepository.save(c1);
-    }
 }
---
+++ back-end/src/main/java/com/janioofi/helpdesk/services/DBService.java
@@ -0,0 +1,44 @@
+package com.janioofi.helpdesk.services;
+
+import com.janioofi.helpdesk.domain.Chamado;
+import com.janioofi.helpdesk.domain.Cliente;
+import com.janioofi.helpdesk.domain.Tecnico;
+import com.janioofi.helpdesk.domain.enums.Perfil;
+import com.janioofi.helpdesk.domain.enums.Prioridade;
+import com.janioofi.helpdesk.domain.enums.Status;
+import com.janioofi.helpdesk.repositories.ChamadoRepository;
+import com.janioofi.helpdesk.repositories.ClienteRepository;
+import com.janioofi.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+public class DBService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public void instanciaDB(){
+        Tecnico tecnico = new Tecnico(null,"Jona Silveira", "66232536100", "jonas@gmail.com", "teste");
+        tecnico.addPefil(Perfil.ADMIN);
+
+        Cliente cliente = new Cliente(null, "Rubens Nascimento", "63185578147", "rubens@gmail.com", "teste");
+
+        Chamado c1 = new Chamado();
+        c1.setCliente(cliente);
+        c1.setTecnico(tecnico);
+        c1.setPrioridade(Prioridade.MEDIA);
+        c1.setTitulo("Ajustar Impressora");
+        c1.setStatus(Status.ANDAMENTO);
+        c1.setDataAbertura(LocalDate.now());
+        c1.setObservacoes("Testar");
+        tecnicoRepository.save(tecnico);
+        clienteRepository.save(cliente);
+        chamadoRepository.save(c1);
+    }
+}
