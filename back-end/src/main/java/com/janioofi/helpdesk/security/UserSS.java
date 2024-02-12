78585fa91eeb69d54daa0eccc471caae9f084221
Jânio <janioofi@gmail.com>
2024-02-12 20:42:18 -0300

    Autenticação com JWT sendo implementada

---
+++ back-end/src/main/java/com/janioofi/helpdesk/security/UserSS.java
@@ -0,0 +1,66 @@
+package com.janioofi.helpdesk.security;
+
+import com.janioofi.helpdesk.domain.enums.Perfil;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.validation.constraints.Email;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserSS implements UserDetails {
+
+    private Integer id;
+    @Email
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSS(Integer id, String email, String password, Set<Perfil> perfis) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
---
+++ back-end/src/main/java/com/janioofi/helpdesk/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.janioofi.helpdesk.services;
+
+import com.janioofi.helpdesk.domain.models.Pessoa;
+import com.janioofi.helpdesk.exceptions.RecordNotFoundException;
+import com.janioofi.helpdesk.repositories.PessoaRepository;
+import com.janioofi.helpdesk.security.UserSS;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final PessoaRepository pessoaRepository;
+
+    public UserDetailsServiceImpl(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Pessoa user = pessoaRepository.findByEmail(email).orElseThrow(() -> new RecordNotFoundException("Nenhuma pessa encontrada com esse email: " + email));
+        return new UserSS(user.getId_pessoa(), user.getEmail(),user.getSenha(), user.getPerfis());
+    }
+}
