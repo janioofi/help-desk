877e9e6a40b265216231db8170e3c33d85d0ed7d
Jânio <janioofi@gmail.com>
2024-03-04 18:54:06 -0300

    Implementando tabela no componente tecnico

--- help-desk-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ help-desk-front/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Tecnico } from '../../../models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatPaginatorModule],
   templateUrl: './tecnico-list.component.html',
   styleUrl: './tecnico-list.component.css'
 })
 export class TecnicoListComponent {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Jânio Filho',
+      cpf: '523152678821',
+      email: 'janio@gmail.com',
+      senha: '1235',
+      perfis: ['0'],
+      dataCriacao: '04/03'
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
