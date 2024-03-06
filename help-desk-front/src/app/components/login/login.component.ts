0d26ef2a93a717aca5364bd2d1ced840623924bc
Jânio <janioofi@gmail.com>
2024-03-06 18:10:48 -0300

    HTML e CSS para Login

--- help-desk-front/src/app/components/login/login.component.ts
+++ help-desk-front/src/app/components/login/login.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatDividerModule, 
+    MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
