0a5c5418a8b0843003b31ae635adeeabf44cb436
Jânio <janioofi@gmail.com>
2024-03-06 18:29:03 -0300

    validação de formulários para login

--- help-desk-front/src/app/components/login/login.component.ts
+++ help-desk-front/src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, Validators, ReactiveFormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Credenciais } from '../../models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -15,10 +16,35 @@ import { MatInputModule } from '@angular/material/input';
     MatInputModule, 
     MatButtonModule, 
     MatDividerModule, 
-    MatIconModule],
+    MatIconModule,
+    ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  validaCampos(): boolean{
+    if(this.email.valid && this.senha.valid){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
 }
