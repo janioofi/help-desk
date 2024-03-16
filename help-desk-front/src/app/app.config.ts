e725d3b24554ea7f15368b734a976e37d7321946
Jânio <janioofi@gmail.com>
2024-03-15 22:31:53 -0300

    interceptor para requisições http

--- help-desk-front/src/app/app.config.ts
+++ help-desk-front/src/app/app.config.ts
@@ -4,8 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideToastr } from 'ngx-toastr';
-import { HttpHeaderResponse, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideToastr(), provideHttpClient(), HttpHeaderResponse]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideToastr(), provideHttpClient(withInterceptors([authInterceptor]))]
 };
