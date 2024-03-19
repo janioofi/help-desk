c8bf86afb9edfb337264404aa4d704320142d8c3
Jânio <janioofi@gmail.com>
2024-03-18 21:40:45 -0300

    Componente Tecnico Create criado

---
+++ help-desk-front/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './tecnico-create.component.html',
+  styleUrl: './tecnico-create.component.css'
+})
+export class TecnicoCreateComponent {
+
+}
