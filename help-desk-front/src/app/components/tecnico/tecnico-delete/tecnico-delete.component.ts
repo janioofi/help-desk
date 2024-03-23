287df1f02bfae19175f6bd04a9d8f93ef17d33ee
Jânio <janioofi@gmail.com>
2024-03-23 12:08:02 -0300

    Componente TECNICO-DELETE criado

---
+++ help-desk-front/src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './tecnico-delete.component.html',
+  styleUrl: './tecnico-delete.component.css'
+})
+export class TecnicoDeleteComponent {
+
+}
