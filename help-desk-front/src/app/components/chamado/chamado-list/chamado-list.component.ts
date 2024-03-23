51acd2fe44b29e9229f5487f7c927ca0dd1bc823
Jânio <janioofi@gmail.com>
2024-03-23 16:25:15 -0300

    componente CHAMADO-LIST criado

---
+++ help-desk-front/src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './chamado-list.component.html',
+  styleUrl: './chamado-list.component.css'
+})
+export class ChamadoListComponent {
+
+}
