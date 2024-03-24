08ba4d8d688fca031ef61fc27e9800a499d91b8e
Jânio <janioofi@gmail.com>
2024-03-24 14:37:11 -0300

    Componente CHAMADO-CREATE criado

---
+++ help-desk-front/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './chamado-create.component.html',
+  styleUrl: './chamado-create.component.css'
+})
+export class ChamadoCreateComponent {
+
+}
