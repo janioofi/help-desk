90ff8b1e8bd42b0da4d1f087f1d8acb1a409ff8a
Jânio <janioofi@gmail.com>
2024-03-25 18:40:30 -0300

    Ajustes no HTML do componente chamado updat

--- help-desk-front/src/app/components/nav/nav.component.ts
+++ help-desk-front/src/app/components/nav/nav.component.ts
@@ -25,7 +25,7 @@ export class NavComponent implements OnInit {
   constructor(private router: Router, private authService: AuthService, private toast: ToastrService){}
 
   ngOnInit(): void{
-    this.router.navigate(['chamados/update/1'])
+    this.router.navigate(['home'])
   }
 
   logout(){
