e73d75f35a22280604f945ab0a3d0140038aa084
Jânio <janioofi@gmail.com>
2024-03-25 18:43:01 -0300

    componente chamado  read criado

---
+++ help-desk-front/src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-read',
+  standalone: true,
+  imports: [],
+  templateUrl: './chamado-read.component.html',
+  styleUrl: './chamado-read.component.css'
+})
+export class ChamadoReadComponent {
+
+}
