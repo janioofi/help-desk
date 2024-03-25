1ce994fe2f663936ab045914d100b5cb3a63ae6b
Jânio <janioofi@gmail.com>
2024-03-25 17:55:19 -0300

    Componente chamado update  criado

---
+++ help-desk-front/src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './chamado-update.component.html',
+  styleUrl: './chamado-update.component.css'
+})
+export class ChamadoUpdateComponent {
+
+}
